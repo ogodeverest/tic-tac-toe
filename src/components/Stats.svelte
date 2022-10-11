eb7cfbfd7a8fa4c1017642c11b4e8f82129522b2
Everest Gjonaj <ogodeverest@192.168.1.2>
2022-10-11 16:40:28 +0200

    Finished stats component logic and scss

--- src/components/Stats.svelte
+++ src/components/Stats.svelte
@@ -1,18 +1,38 @@
 <script lang="ts">
+  import type GameState from "src/utils/GameState";
+  import type { Mark } from "src/utils/Player";
+  import store from "../store";
+  import CPU from "../utils/CPU";
+
+  function renderLabel(state: GameState, mark: Mark): string {
+    if (state.players.get(mark) instanceof CPU) return "(CPU)";
+    if (state.players.get(mark) === state.current) return "(YOU)";
+    return "";
+  }
 </script>
 
 <footer class="stats">
-  <div class="stat stat--wins">
-    <p class="stat__key">X (You)</p>
-    <span class="stat__number"> 6 </span>
+  <div class="stat stat--x">
+    <p class="stat__key">
+      X
+      {renderLabel($store, "X")}
+    </p>
+    <span class="stat__number">
+      {$store.players.get("X").wins}
+    </span>
   </div>
   <div class="stat stat--ties">
     <p class="stat__key">Ties</p>
-    <span class="stat__number">0</span>
+    <span class="stat__number">{$store.players.get("X").ties} </span>
   </div>
-  <div class="stat stat--losses">
-    <p class="stat__key">O (CPU)</p>
-    <span class="stat__number">10</span>
+  <div class="stat stat--o">
+    <p class="stat__key">
+      O
+      {renderLabel($store, "O")}
+    </p>
+    <span class="stat__number">
+      {$store.players.get("O").wins}
+    </span>
   </div>
 </footer>
 
@@ -24,18 +44,18 @@
     font-weight: 400;
 
     .stat {
-      @include tile(var(--clr-warm));
+      @include box(var(--clr-default));
       flex-direction: column;
       padding: 0.5em 1em;
 
-      &--wins {
+      &--x {
         background-color: var(--clr-cold);
       }
 
       &--ties {
         background-color: var(--clr-neutral);
       }
-      &--loses {
+      &--o {
         background-color: var(--clr-warm);
       }
 
