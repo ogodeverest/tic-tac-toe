df3a03ad459b64b8de3936ebde3320cb3ce0a5f2
Everest Gjonaj <ogodeverest@192.168.1.2>
2022-10-11 16:41:00 +0200

    Added store to Grid component

--- src/components/Grid.svelte
+++ src/components/Grid.svelte
@@ -1,37 +1,32 @@
 <script lang="ts">
-  import CircleIcon from "./CircleIcon.svelte";
-  import CrossIcon from "./CrossIcon.svelte";
-  import Tile from "./Tile.svelte";
+  import CircleMark from "./icons/CircleMark.svelte";
+  import CrossMark from "./icons/CrossMark.svelte";
+  import Button from "./Button.svelte";
+  import store from "../store";
+  import type Position from "../models/Position.inferface";
+
+  const markSize = "clamp(2.5rem,5vw,6rem)";
+  function handleClick(position: Position) {
+    store.mark(position);
+  }
 </script>
 
 <section class="grid">
-  <Tile>
-    <CircleIcon size="clamp(2.5rem,5vw,6rem)" color="var(--clr-warm)" />
-  </Tile>
-  <Tile>
-    <CrossIcon size="clamp(2.5rem,5vw,6rem)" color="var(--clr-cold)" />
-  </Tile>
-  <Tile>
-    <CircleIcon size="clamp(2.5rem,5vw,6rem)" color="var(--clr-warm)" />
-  </Tile>
-  <Tile>
-    <CircleIcon size="clamp(2.5rem,5vw,6rem)" color="var(--clr-warm)" />
-  </Tile>
-  <Tile>
-    <CrossIcon size="clamp(2.5rem,5vw,6rem)" color="var(--clr-cold)" />
-  </Tile>
-  <Tile>
-    <CircleIcon size="clamp(2.5rem,5vw,6rem)" color="var(--clr-warm)" />
-  </Tile>
-  <Tile>
-    <CircleIcon size="clamp(2.5rem,5vw,6rem)" color="var(--clr-warm)" />
-  </Tile>
-  <Tile>
-    <CrossIcon size="clamp(2.5rem,5vw,6rem)" color="var(--clr-cold)" />
-  </Tile>
-  <Tile>
-    <CircleIcon size="clamp(2.5rem,5vw,6rem)" color="var(--clr-warm)" />
-  </Tile>
+  {#each $store.grid as row, y}
+    {#each row as tile, x}
+      <Button
+        on:click={() => handleClick({ x, y })}
+        disabled={!!tile || $store.stalled || $store.finished}
+        style="aspect-ratio:1;"
+      >
+        {#if tile === $store.players.get("O")}
+          <CircleMark size={markSize} color="var(--clr-warm)" />
+        {:else if tile === $store.players.get("X")}
+          <CrossMark size={markSize} color="var(--clr-cold)" />
+        {/if}
+      </Button>
+    {/each}
+  {/each}
 </section>
 
 <style lang="scss">
